d16ea13cf367688652c75187e7d5a51981ff6226
lunatechdevlab <lunatechdevlab@gmail.com>
2023-11-06 14:47:32 +0100

    Google analytics changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,6 +32,21 @@ export default function Home() {
           key="desc"
         />
         <link rel="icon" href="/images/favicon.png" sizes="any" />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+    page_path: window.location.pathname
+  });`,
+          }}
+        ></script>
         <meta name="language" content="en" />
 
         <meta
