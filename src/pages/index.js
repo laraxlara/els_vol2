737a1d108db4180decb91a7bf301bed58a459bb2
lunatechdevlab <lunatechdevlab@gmail.com>
2023-11-13 12:14:03 +0100

    gtm ads

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,6 +73,22 @@ export default function Home() {
   });`,
         }}
       ></script>
+
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ADS}`}
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ADS}', {
+    page_path: window.location.pathname
+  });`,
+        }}
+      ></script>
       <Layout>
         <Hero />
         <Main />
