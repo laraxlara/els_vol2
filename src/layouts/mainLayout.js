61f184857726ca02c75c11414fdf1588b945a24a
lunatechdevlab <lunatechdevlab@gmail.com>
2023-11-21 18:48:42 +0100

    gtm

--- src/layouts/mainLayout.js
+++ src/layouts/mainLayout.js
@@ -35,6 +35,25 @@ const Layout = ({ children }) => {
     <>
       <Head>
         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}');
+        `,
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
+        {/* <script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         ></script>
@@ -73,7 +92,7 @@ const Layout = ({ children }) => {
               });
             `,
           }}
-        />
+        /> */}
       </Head>
       <div className="home-page-container">
         <div className="h-[200px] py-[50px]">
