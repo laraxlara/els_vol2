3131cd7f53adfcb19d9632eebdfff8124a10e8a2
laraxlara <laraa.celic7@gmail.com>
2024-03-19 20:08:56 +0100

    video

--- src/layouts/mainLayout.js
+++ src/layouts/mainLayout.js
@@ -9,79 +9,10 @@ import { PropTypes } from "prop-types";
 const Layout = ({ children }) => {
   return (
     <>
-      <Head>
-        {/* <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}');
-        `,
-          }}
-        />
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}"
-                height="0" width="0" style="display:none;visibility:hidden"></iframe>
-            `,
-          }}
-        />
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-    page_path: window.location.pathname
-  });`,
-          }}
-        ></script>
-
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ADS}`}
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ADS}', {
-    page_path: window.location.pathname
-  });`,
-          }}
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              gtag('event', 'conversion', {
-                'send_to': '${process.env.NEXT_PUBLIC_CONVERSION_ID}/${process.env.NEXT_PUBLIC_CONVERSION_LABEL}'
-              });
-            `,
-          }}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              gtag('event', 'conversion', {
-                'send_to': '${process.env.NEXT_PUBLIC_CONVERSION_ID}/${process.env.NEXT_PUBLIC_CONVERSION_LABEL2}'
-              });
-            `,
-          }}
-        /> */}
-      </Head>
+      <Head></Head>
       <div className="home-page-container">
         <div className="h-[200px] py-[50px]">
-          <video autoPlay loop muted src={"/videos/video.mp4"} />
+          <video autoPlay loop muted playsInline src={"/videos/video.mp4"} />
         </div>
         <Navbar />
         <main className="relative z-1">{children}</main>
